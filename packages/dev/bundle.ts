e5b2f9f0cbc8e37d0e7dd3ffd0241eb7bc80c00c
liontari.ai <sj@liontari.ai>
2025-07-16 18:08:46 +0200

    make sure graphql is not bundled to avoid 'multiple graphql instances' error

--- packages/dev/bundle.ts
+++ packages/dev/bundle.ts
@@ -3,7 +3,11 @@ import { build } from "bun";
 // esm output
 await build({
     entrypoints: ["./src/index.ts"],
-    external: Object.keys(require("./package.json").dependencies),
+    external: Object.keys(require("./package.json").dependencies).concat([
+        "graphql",
+        "graphql-sse",
+        "@graphql-tools/schema",
+    ]),
     outdir: "./dist/esm",
     format: "esm",
     banner: "#!/usr/bin/env bun",
