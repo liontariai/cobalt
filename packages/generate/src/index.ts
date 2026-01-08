7e3587a29920ae6c9abff1e8835aa8f7a1a31932
liontari.ai <sj@liontari.ai>
2026-01-08 13:20:59 +0100

    implement 'resolveType' function for Union types with automatic & type-safe code-gen template for user When a resolver returns a union type, in the resolver's "endpoint" file the fully typed 'resolveType' function is added with a comment explaining that the user should complete the implementation, so that the types are valid -> thus guiding the user to correctly discriminate the types for the graphql resolver

--- packages/generate/src/index.ts
+++ packages/generate/src/index.ts
@@ -1,8 +1,9 @@
+import fs from "fs";
 import path from "path";
 
 import { Collector, gatherMeta } from "./collector";
 import { GeneratorSchemaGQL } from "./schema/graphql";
-import type { CodegenOptions, SchemaMeta } from "./collector/types";
+import type { CodegenOptions, SchemaMeta, TypeMeta } from "./collector/types";
 
 export class Generator {
     constructor() {}
@@ -10,9 +11,13 @@ export class Generator {
     private generateTSTypesFile(schemaMeta: SchemaMeta) {
         const typesFiles: Record<string, string> = {};
 
-        for (const extType of schemaMeta.extendedTypes) {
+        const typeNames = new Set<string>();
+        const unionTypeNames = new Set<string>();
+        const unionTypeImports = new Set<string>();
+
+        const makeTypeFields = (t: TypeMeta) => {
             const fieldDefs: string[] = [];
-            for (const field of extType.fields) {
+            for (const field of t.fields) {
                 let fieldArgs = "";
                 if (field.args?.length) {
                     fieldArgs = `(${field.args.map(
@@ -24,6 +29,11 @@ export class Generator {
                     `${field.description ? `/*\n${field.description}\n*/` : ""}${field.name}: ${fieldArgs}${field.type.tsTypeName}`,
                 );
             }
+            return fieldDefs;
+        };
+
+        for (const extType of schemaMeta.extendedTypes) {
+            const fieldDefs = makeTypeFields(extType);
             const tsTypeFileContent = `
                 ${extType.description ? `/*\n${extType.description}\n*/` : ""}
                 export type ${extType.name} = {
@@ -32,24 +42,113 @@ export class Generator {
             `;
 
             typesFiles[extType.name] = tsTypeFileContent;
+            typeNames.add(extType.name);
+        }
+
+        for (const unionType of schemaMeta.types.filter((t) => t.isUnion)) {
+            if (unionType.isInput) continue;
+
+            const makeTypeFile = (type: TypeMeta) => {
+                const types: string[] = [];
+                const fieldDefs = makeTypeFields(type);
+                const tsTypeFileContent = `
+                    ${type.description ? `/*\n${type.description}\n*/` : ""}
+                    export type ${type.name.replaceAll("!", "")} = {
+                        ${fieldDefs.join("\n")}
+                    }
+                `;
+
+                typesFiles[type.name.replaceAll("!", "")] = tsTypeFileContent;
+                types.push(type.name.replaceAll("!", ""));
+                return type.name.replaceAll("!", "");
+            };
+
+            const typeDefs: string[] = [];
+            for (const type of unionType.possibleTypes) {
+                const t = makeTypeFile(type);
+                typeDefs.push(t);
+                unionTypeImports.add(t);
+            }
+
+            const tsTypeFileContent = `
+                ${typeDefs
+                    .map(
+                        (typeName) =>
+                            `import type { ${typeName} } from "./${typeName}";`,
+                    )
+                    .join("\n")}
+                
+                ${unionType.description ? `/*\n${unionType.description}\n*/` : ""}
+                export type ${unionType.name.replaceAll("!", "")} = ${typeDefs.join(" | ")};
+                export type ${unionType.name.replaceAll("!", "")}ResolveToTypename = ${typeDefs.map((td) => `"${td}"`).join(" | ")};
+            `;
+            typesFiles[unionType.name.replaceAll("!", "")] = tsTypeFileContent;
+
+            unionTypeNames.add(unionType.name.replaceAll("!", ""));
         }
 
         typesFiles["index"] = `
-            ${Object.keys(typesFiles)
-                .map(
-                    (typeName) =>
-                        `import type { ${typeName} } from "./${typeName}";`,
-                )
-                .join("\n")}
-
             export type Types = {
-                ${Object.keys(typesFiles)
-                    .map((typeName) => `${typeName}: ${typeName},`)
+                ${Array.from(typeNames)
+                    .map(
+                        (typeName) =>
+                            `${typeName}: import("./${typeName}").${typeName},`,
+                    )
+                    .join("\n")}
+            }
+            export type Unions = {
+                ${Array.from(unionTypeNames)
+                    .map(
+                        (typeName) =>
+                            `${typeName}: import("./${typeName}").${typeName},`,
+                    )
+                    .join("\n")}
+            }
+            export type UnionsResolveToTypename = {
+                ${Array.from(unionTypeNames)
+                    .map(
+                        (typeName) =>
+                            `${typeName}: import("./${typeName}").${typeName}ResolveToTypename,`,
+                    )
                     .join("\n")}
             }
         `;
 
         return typesFiles;
+    }
+
+    private syncResolveTypeFunctions(schemaMeta: SchemaMeta) {
+        for (const operation of schemaMeta.operations) {
+            if (operation.type.isUnion) {
+                const pureOpTypeName = operation.type.name.replaceAll("!", "");
+
+                const reg = new RegExp(
+                    `${operation.operation}\.resolveType(.*)\{`,
+                );
+                let code = fs.readFileSync(operation.file, "utf-8");
+                if (!reg.test(code)) {
+                    code += [
+                        "",
+                        "// Your resolver returns a Union Type. Therefore you must provide a resolveType function that resolves the abstract union type to a concrete type by it's typename.",
+                        "// The following fully-typed template has been added by cobalt. Please make sure it resolves correctly, like the types indicate.",
+                        `${operation.operation}.resolveType = (value: import("$$types").Unions["${pureOpTypeName}"]): import("$$types").UnionsResolveToTypename["${pureOpTypeName}"] => {`,
+                        `    switch(value){`,
+                        `        default:`,
+                        `            return "";`,
+                        `    }`,
+                        `};`,
+                    ].join("\n");
+                    fs.writeFileSync(operation.file, code, "utf-8");
+                } else {
+                    // If resolveType is present, update the function to use new value and return types
+                    code = code.replace(
+                        new RegExp(`${operation.operation}\.resolveType(.*)\{`),
+                        `${operation.operation}.resolveType = (value: import("$$$types").Unions["${pureOpTypeName}"]): import("$$$types").UnionsResolveToTypename["${pureOpTypeName}"] => {`,
+                    );
+                    fs.writeFileSync(operation.file, code, "utf-8");
+                }
+            }
+        }
     }
 
     public async generate(operationsDir: string, options: CodegenOptions = {}) {
@@ -60,6 +159,8 @@ export class Generator {
         const schemaMeta = gatherMeta(operationsDir, options, collector);
 
         const typesFiles = this.generateTSTypesFile(schemaMeta);
+        this.syncResolveTypeFunctions(schemaMeta);
+
         const schema = new GeneratorSchemaGQL(schemaMeta).generateSchema();
 
         const opsImports: string[] = [];
@@ -68,6 +169,7 @@ export class Generator {
         const mutations: string[] = [];
         const subscriptions: string[] = [];
         const extendedTypes: string[] = [];
+        const unionOperations: { typeName: string; importName: string }[] = [];
         for (const operation of schemaMeta.operations) {
             opsImports.push(
                 `import { ${operation.operation} as ${operation.name} } from "${path.resolve(operationsDir, operation.file)}";`,
@@ -79,6 +181,19 @@ export class Generator {
             } else if (operation.operation === "Subscription") {
                 subscriptions.push(operation.name);
             }
+        }
+
+        for (const unionOperation of schemaMeta.operations.filter(
+            (o) => o.type.isUnion,
+        )) {
+            const importName = `U_${unionOperation.operation}_${unionOperation.type.name.replaceAll("!", "")}`;
+            opsImports.push(
+                `import { ${unionOperation.operation} as ${importName} } from "${path.resolve(operationsDir, unionOperation.file)}";`,
+            );
+            unionOperations.push({
+                typeName: unionOperation.type.name.replaceAll("!", ""),
+                importName,
+            });
         }
 
         for (const extType of schemaMeta.extendedTypes) {
@@ -115,9 +230,24 @@ export class Generator {
         const ExtendedTypes = extendedTypes
             .map(
                 (name) =>
-                    `export const ${name} = Object.fromEntries(Object.entries(T_${name}).map(([fieldName, fieldFunc]) => [fieldName, makeGraphQLFieldResolver(fieldFunc)]));`,
+                    `export const ${name} = Object.fromEntries([
+                        ...Object.entries(T_${name}).map(([fieldName, fieldFunc]) => [fieldName, makeGraphQLFieldResolver(fieldFunc)]),
+                        ["__typename", makeGraphQLFieldResolver(() => "${name}")],
+                    ]);`,
             )
             .join("\n");
+
+        const UnionTypes = unionOperations
+            .map((u) => {
+                if (extendedTypes.includes(u.typeName)) {
+                    return `${u.typeName}.__resolveType = ${u.importName}.resolveType;`;
+                }
+                return `export const ${u.typeName} = {
+                        __resolveType: ${u.importName}.resolveType,
+                    }`;
+            })
+            .join("\n");
+
         const code = [
             `import { makeGraphQLResolverFn, makeGraphQLFieldResolver } from "@cobalt27/runtime";`,
             ...opsImports,
@@ -126,6 +256,7 @@ export class Generator {
             mutations.length && Mutation,
             subscriptions.length && Subscription,
             extendedTypes.length && ExtendedTypes,
+            unionOperations.length && UnionTypes,
         ]
             .filter(Boolean)
             .join("\n");
